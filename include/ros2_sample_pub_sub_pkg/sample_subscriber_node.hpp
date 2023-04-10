580cf531d360d2e3f9d7fd82f3d014d26f4361bf
aiden3 <aiden3@clobot.co.kr>
2023-04-10 07:01:23 -0700

    add; subscriber

---
+++ include/ros2_sample_pub_sub_pkg/sample_subscriber_node.hpp
@@ -0,0 +1,16 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#pragma once
+
+class SampleSubscriberNode : public rclcpp::Node
+{
+private:
+	rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_hello_world_;
+
+public:
+	SampleSubscriberNode();
+
+private:
+	void OnSubscribed_hello_world();
+};
---
+++ src/sample_subscriber_node.cpp
@@ -0,0 +1,30 @@
+#include "ros2_sample_pub_sub_pkg/sample_subscriber_node.hpp"
+
+#include <functional>
+#include <memory>
+
+SampleSubscriberNode::SampleSubscriberNode()
+	:Node("sample_subscriber_node")
+{
+	auto qos_profile = rclcpp::Qos(rclcpp::KeepLast(10));
+	sub_hello_world_ = this->create_subscription<std_msgs::msg::String>(
+		"/helloworld",
+		qos_profile,
+		std::bind(&SampleSubscriberNode::OnSubscribed_hello_world, this, std::placeholders::_1));
+}
+
+void SampleSubscriberNode::OnSubscribed_hello_world(const std_msgs::msg::String::SharedPtr hello_world)
+{
+	RCLCPP_INFO(this->get_logger(), "Received message: '%s'", hello_world->data.c_str());
+}
+
+
+int main(int argc, char* argv[])
+{
+	rclcpp::init(argc, argv);
+	auto node = std::make_shared<SampleSubscriberNode>();
+	rclcpp::spin(node);
+	rclcpp::shutdown();
+
+	return 0;
+}
